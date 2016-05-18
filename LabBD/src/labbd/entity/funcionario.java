3fae0e343620cd2b6e7caf0c833c49b8c23249ef
franciscomarchini <francisco.marchini@gmail.com>
2016-05-18 01:19:05 -0300

    git_ignore

---
+++ LabBD/src/labbd/entity/funcionario.java
@@ -0,0 +1,108 @@
+package labbd.entity;
+
+import java.util.Date;
+
+public class funcionario {
+	
+	private String nome;
+	private Date datanasc;
+	private int rg;
+	private String cpf;
+	private String logradouro;
+	private int num;
+	private String complemento;
+	private int cep;
+	private String bairro;
+	private String cidade;
+	private String uf;
+	private String telefone;
+	private String email;
+	private double salario;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Date getDatanasc() {
+		return datanasc;
+	}
+	public void setDatanasc(Date datanasc) {
+		this.datanasc = datanasc;
+	}
+	public int getRg() {
+		return rg;
+	}
+	public void setRg(int rg) {
+		this.rg = rg;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public int getCep() {
+		return cep;
+	}
+	public void setCep(int cep) {
+		this.cep = cep;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getUf() {
+		return uf;
+	}
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	
+	
+}
